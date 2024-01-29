6e78ccda1b9058e1c0fa18082bc2c9837bc21176
Adrián <adrsabper@gmail.com>
2024-01-29 22:57:00 +0100

    .

--- ActividadesOrdenadas/Actividad_2.py
+++ ActividadesOrdenadas/Actividad_2.py
@@ -2,6 +2,6 @@
 
 x = " Bienvenidos a crase "
 x = x.strip()
-print("Cadena sin espacios en blanco al principio y final:", x)
+print("Cadena sin espacios en blanco al principio y final:" + x)
 x = x.replace("r", "l")
 print("Cadena reemplazando 'r' por 'l':", x)
